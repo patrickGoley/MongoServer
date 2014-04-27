840c845e1b87dd946e24d03a95134c2db545038f
Patrick Goley <patrick.goley@gmail.com>
2014-04-26 20:44:29 -0500

    hooked up collection driver to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http'),
-	express = require('express');
-	path = require('path');
+	express = require('express'),
+	path = require('path'),
+	MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server,
+	CollectionDriver = require('./collectionDriver').CollectionDriver;
 
 var app = express();
 
@@ -8,20 +11,65 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var mongoHost = 'localHost';
+var mongoPort = 27017;
+var collectionDriver;
+
+//set up the mongodb collection driver
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
+mongoClient.open(function(err, mongoClient) {
+	if (!mongoClient) {
+		console.error("Error! Exiting... Must start MongoDB first");
+		process.exit(1);
+	}
+	var db = mongoClient.db("MyDatabase");
+	collectionDriver = new CollectionDriver(db);
+});
+
+//serve static files under the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function (req, res) {
-	res.send('<html><body><h1>Hello World</h1></body></html>');
+//return all entities for the given collection
+app.get('/:collection', function(req, res) {
+	var params = req.params;
+	collectionDriver.findAll(req.params.collection, function(error, objs) {
+		if (error) {
+			res.send(400, error);
+		} else {
+			if (req.accepts('html')) {
+				res.render('data', {objects: objs, collection: req.params.collection});
+			} else {
+				res.set('Content-Type', 'application/json');
+				res.sent(200, objs);
+			}
+		}
+	});
 });
 
-app.get('/:a?/:b?/:c?', function (req, res) {
-	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+//return a specific entity in a collection
+app.get('/:collection/:entity', function(req, res) {
+	var params = req.params;
+	var entity = params.entity;
+	var collection = params.collection;
+	if (entity) {
+		collectionDriver.get(collection, entity, function(error, objs) {
+			if (error) {
+				res.send(400, error);
+			} else {
+				res.send(200, objs);
+			}
+		});
+	} else {
+		res.send(400, {error: 'bad url', url: req.url});
+	}
 });
 
+//return a 404 page for unhandled requests
 app.use(function (req,res) {
     res.render('404', {url:req.url}); 
 });
-	
+
+//start the http server
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
