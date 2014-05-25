be1decc1510467d1730e0d2d59c48212606e1f68
Patrick Goley <patrick.goley@gmail.com>
2014-05-24 19:23:56 -0500

    added paw test cases for sharing

--- index.js
+++ index.js
@@ -34,36 +34,6 @@ mongo.Db.connect(mongoUri, function(err, db) {
 	collectionDriver = new CollectionDriver(db);
 });
 
-//return all entities for the given collection
-app.get('/:collection', function(req, res) {
-	var params = req.params;
-	collectionDriver.findAll(req.params.collection, function(error, objs) {
-		if (error) {
-			res.send(400, error);
-		} else {
-			if (req.accepts('html')) {
-				res.render('data', {objects: objs, collection: req.params.collection});
-			} else {
-				res.set('Content-Type', 'application/json');
-				res.sent(200, objs);
-			}
-		}
-	});
-});
-
-//post a new object to a collection
-app.post('/:collection', function(req, res) {
-	var object = req.body;
-	var collection = req.params.collection;
-	collectionDriver.save(collection, object, function(err, docs) {
-		if (err) {
-			res.send(400, err);
-		} else {
-			res.send(201, docs);
-		}
-	});
-});
-
 //post a Share object
 app.post('/shares/:type', function(req, res) {
 	var object = req.body;
@@ -113,18 +83,35 @@ app.get('/shares/:type/:userId', function(req, res) {
 	});
 });
 
-app.post('shares/:type/', function(req, res) {
-	var object = req.body;
-	var collection = req.params.type;
-	var userId = req.params.userId;
-	collectionDriver.save(collection, object, function(error, docs) {
+//return all entities for the given collection
+app.get('/:collection', function(req, res) {
+	var params = req.params;
+	collectionDriver.findAll(req.params.collection, function(error, objs) {
 		if (error) {
+			res.send(400, error);
+		} else {
+			if (req.accepts('html')) {
+				res.render('data', {objects: objs, collection: req.params.collection});
+			} else {
+				res.set('Content-Type', 'application/json');
+				res.sent(200, objs);
+			}
+		}
+	});
+});
+
+//post a new object to a collection
+app.post('/:collection', function(req, res) {
+	var object = req.body;
+	var collection = req.params.collection;
+	collectionDriver.save(collection, object, function(err, docs) {
+		if (err) {
 			res.send(400, err);
 		} else {
 			res.send(201, docs);
 		}
-	})
-})
+	});
+});
 
 //update an existing object
 app.put('/:collection/:entity', function(req, res) {
